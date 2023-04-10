36eff3cad3a648b8934bb852a2a7a9533c70ebdf
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-04-10 13:17:08 -0600

    in class demo

--- src/App.js
+++ src/App.js
@@ -2,29 +2,49 @@ import "./App.css";
 import { useState } from "react";
 import Movielist from "./components/movielist";
 
-
 function App() {
 	//let searchTerm = ""
 	const [searchTerm, setSearchTerm] = useState("");
+	const apiKey = e09dfec3
+	const [movies, setMovies] = useState([])
 
-	const movies = [
-		{
-			title: "title",
-			year: "year",
-			genre: "genre",
-      imdbTD: "1"
-		},
-		{
-			title: "title2",
-			year: "year2",
-			genre: "genre2",
-      imdbTD: "12"
-		},
-	];
+	const movieApiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
+	// const movies = [
+	// 	{
+	// 		title: "title",
+	// 		year: "year",
+	// 		genre: "genre",
+	// 		imdbTD: "1",
+	// 	},
+	// 	{
+	// 		title: "title2",
+	// 		year: "year2",
+	// 		genre: "genre2",
+	// 		imdbTD: "12",
+	// 	},
+	// ];
 	const handleSearch = (event) => {
 		event.preventDefault();
 		console.log("user entered " + searchTerm);
+		
+		setMovies([]);
+		if(searchTerm && searchTerm != ""){
+			loadMovies();
+		}
 	};
+
+	//call api
+	//if/when pizza gets back tell react
+	const loadMovies = () =>{
+		fetch(movieApiUrl)
+			.then(resp => resp.json())
+			.then(data =>{
+				console.log(data)
+				setMovies(data.Search)
+			})
+			.catch(err => console.log(err))
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -34,13 +54,22 @@ function App() {
 				<form onSubmit={handleSearch}>
 					<label></label>
 					{/* wire up the input box to a local var  */}
-					<input type="text"	placeholder="move title"	value={searchTerm}	onChange={(evt) => setSearchTerm(evt.target.value)}></input>
+					<input
+						type="text"
+						placeholder="move title"
+						value={searchTerm}
+						onChange={(evt) => setSearchTerm(evt.target.value)}
+					></input>
 					<button type="submit">Go</button>
 				</form>
 			</section>
-      <section>
-        <Movielist movieData={movies} />
-      </section>
+			<section>
+				{
+					//ternary bool ? if true : false
+					movies && movies.length > 0? <Movielist movieData={movies} />: <div>No movies found. You must enter a valid search term</div>
+				}
+				
+			</section>
 			<footer>&copy; Copyright - Katie King - 2023</footer>
 		</div>
 	);
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -3,17 +3,16 @@ import React from "react";
 //generates html showing a list of movies
 //parent will pass an array via props
 const Movielist = (props) => {
+	const showMovieDetails = (movie) =>{
+		console.log(movie)
+	}
 	return (
 		<div>
 			<h2>Movie Results</h2>
 			<ol>
-				{
-          props.movieData?.map((movie) =>(
-            <li key={movie.imdbTD}>
-              {movie.title}
-            </li>
-          ))
-        }
+				{props.movieData?.map((movie) => (
+					<li key={movie.imdbTD}><a href="#" onClick={showMovieDetails(movie)}>{movie.Title}</a></li>
+				))}
 			</ol>
 		</div>
 	);
